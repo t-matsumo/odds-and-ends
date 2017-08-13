a63304db215e79b98e10b5d3432a8463213b41d6
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-08-13 15:36:02 +0900

    Adjust details2

--- lib/train_information_notification.rb
+++ lib/train_information_notification.rb
@@ -6,6 +6,6 @@ class TrainInformationNotification
       text.concat("*・#{trainInformation.train}*\n　#{trainInformation.status}\n　#{trainInformation.detail}\n\n")
     end
     text.concat('詳細情報：https://transit.yahoo.co.jp/traininfo/area/4/')
-    Notification.notify(ENV['SLACK_TRAIN_NOTIFICATION_CHANNEL'], ENV['SLACK_TRAIN_NOTIFICATION_USER'], text)
+    Notification.notify(ENV['SLACK_TRAIN_NOTIFICATION_CHANNEL'], ENV['SLACK_TRAIN_NOTIFICATION_USER'], text) if text.count("\n") > 1
   end
 end
