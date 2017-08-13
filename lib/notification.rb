d554e1d74d2684f1cbcaf0c100c7b9cb6a572fa8
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-08-13 16:21:31 +0900

    Adjust details2

--- lib/notification.rb
+++ lib/notification.rb
@@ -2,5 +2,9 @@ class Notification
   def self.notify(channel, username, text)
     slack = Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL'], channel: channel, username: username)
     slack.post text
+
+    slack2 = Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL2'], channel: "#train_information", username: username)
+    slack2.post text
+
   end
 end
